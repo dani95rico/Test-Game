be2e23b9f36fb1f220a88e715540771481d57925
DANIEL RICO ESTEBAN <75356826+dani95rico@users.noreply.github.com>
2022-02-15 14:20:42 +0100

    Create Nivel8.java

---
+++ Nivel8.java
@@ -0,0 +1,48 @@
+
+public class Nivel8 {
+	private int [][] matriz=new int[5][5];
+	private int movimientos;
+	private String nombre;
+	public Nivel8()
+	{
+		this.nombre="NIVEL 8";
+		this.movimientos=30;
+		this.matriz[0][0]=0;
+		this.matriz[0][1]=0;
+		this.matriz[0][2]=0;
+		this.matriz[0][3]=0;
+		this.matriz[0][4]=0;
+		this.matriz[1][0]=8;
+		this.matriz[1][1]=0;
+		this.matriz[1][2]=0;
+		this.matriz[1][3]=8;
+		this.matriz[1][4]=0;
+		this.matriz[2][0]=0;
+		this.matriz[2][1]=5;
+		this.matriz[2][2]=0;
+		this.matriz[2][3]=3;
+		this.matriz[2][4]=1;
+		this.matriz[3][0]=0;
+		this.matriz[3][1]=8;
+		this.matriz[3][2]=0;
+		this.matriz[3][3]=8;
+		this.matriz[3][4]=2;
+		this.matriz[4][0]=0;
+		this.matriz[4][1]=8;
+		this.matriz[4][2]=0;
+		this.matriz[4][3]=8;
+		this.matriz[4][4]=4;
+	}
+	public int[][] getMatriz()
+	{
+		return matriz;
+	}
+	public int getMovimientos()
+	{
+		return movimientos;
+	}
+	public String getNombre()
+	{
+		return nombre;
+	}
+}
