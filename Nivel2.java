d59bdb4fe55e3cc53ad035e74d832c914ba3b76c
DANIEL RICO ESTEBAN <75356826+dani95rico@users.noreply.github.com>
2022-02-15 14:18:39 +0100

    Create Nivel2.java

---
+++ Nivel2.java
@@ -0,0 +1,47 @@
+public class Nivel2 {
+	private int [][] matriz=new int[5][5];
+	private int movimientos;
+	private String nombre;
+	public Nivel2()
+	{
+		this.nombre="NIVEL 2";
+		this.movimientos=17;
+		this.matriz[0][0]=1;
+		this.matriz[0][1]=0;
+		this.matriz[0][2]=0;
+		this.matriz[0][3]=0;
+		this.matriz[0][4]=0;
+		this.matriz[1][0]=0;
+		this.matriz[1][1]=0;
+		this.matriz[1][2]=0;
+		this.matriz[1][3]=8;
+		this.matriz[1][4]=2;
+		this.matriz[2][0]=3;
+		this.matriz[2][1]=0;
+		this.matriz[2][2]=0;
+		this.matriz[2][3]=0;
+		this.matriz[2][4]=0;
+		this.matriz[3][0]=0;
+		this.matriz[3][1]=0;
+		this.matriz[3][2]=0;
+		this.matriz[3][3]=0;
+		this.matriz[3][4]=4;
+		this.matriz[4][0]=5;
+		this.matriz[4][1]=0;
+		this.matriz[4][2]=0;
+		this.matriz[4][3]=0;
+		this.matriz[4][4]=0;
+	}
+	public int[][] getMatriz()
+	{
+		return matriz;
+	}
+	public int getMovimientos()
+	{
+		return movimientos;
+	}
+	public String getNombre()
+	{
+		return nombre;
+	}
+}
